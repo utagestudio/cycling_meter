4d5dbd7a24857644f24cf6d35b77cf545055bc6a
utagestudio <utagestudio+github@gmail.com>
2025-07-31 20:05:15 +0900

    **Update font handling and enhance UI design**
    - UIデザインを改善し、新たに太字フォント`Font_bold.ttf`を追加しました。
    - 距離、速度、消費カロリーなどのデータ表示を整理し、視認性を向上。
    - ロゴエリアやフレームデザインを導入。
    - ケイデンスの可視化機能を追加し、データ表示に円グラフを採用。
    - `.gitignore`に`Font_bold.ttf`を追加し、フォント管理を強化。

--- src/cycling_epd.py
+++ src/cycling_epd.py
@@ -48,17 +48,18 @@ class CyclingEPDDisplay:
 
         # Setup FONT
         font_family = 'Font.ttf'
+        font_family_bold = 'Font_bold.ttf'
         self.fonts = {
+            'font12': ImageFont.truetype(os.path.join(picdir, font_family), 12),
             'font15': ImageFont.truetype(os.path.join(picdir, font_family), 15),
-            'font24': ImageFont.truetype(os.path.join(picdir, font_family), 24),
-            'font40': ImageFont.truetype(os.path.join(picdir, font_family), 40)
+            'font15bold': ImageFont.truetype(os.path.join(picdir, font_family_bold), 15),
+            'font40bold': ImageFont.truetype(os.path.join(picdir, font_family_bold), 40)
         }
 
         # Initialize Screen
         self.epd.init_fast()
         self.image = Image.new('1', (self.epd.height, self.epd.width), 255)
         self.draw = ImageDraw.Draw(self.image)
-
 
         # Draw fixed elements
         self.draw_labels()
@@ -68,14 +69,28 @@ class CyclingEPDDisplay:
  
 
     def draw_labels(self):
-        self.draw.text((0,0), 'スピード: ', font = self.fonts['font15'], fill = 0)
-        self.draw.text((105,0), 'km/h', font = self.fonts['font15'], fill = 0)
-        self.draw.text((150,0), '距離: ', font = self.fonts['font15'], fill = 0)
-        self.draw.text((230,0), 'km', font = self.fonts['font15'], fill = 0)
-        self.draw.text((0,105), '時間: ', font = self.fonts['font15'], fill = 0)
-        self.draw.text((0,30), '消費カロリー', font = self.fonts['font15'], fill = 0)
-        self.draw.text((125,70), 'kcal', font = self.fonts['font15'], fill = 0)
- 
+        # frame
+        self.draw.rectangle([(0,0),(250,122)], fill = 0xFF)
+        self.draw.rectangle([(2,2),(246,120)], fill = 0x0)
+        self.draw.rectangle([(4,4),(244,118)], fill = 0xFF)
+
+        #data
+        self.draw.text((6,6), '距離', font = self.fonts['font15'], fill = 0x0)
+        self.draw.text((82,6), 'km', font = self.fonts['font15'], fill = 0x0)
+        self.draw.text((6,101), 'スピード', font = self.fonts['font15'], fill = 0x0)
+        self.draw.text((104,101), 'km/h', font = self.fonts['font15'], fill = 0x0)
+        self.draw.text((150,101), '時間', font = self.fonts['font15'], fill = 0x0)
+        self.draw.text((89,32), '消費カロリー', font = self.fonts['font15'], fill = 0x0)
+        self.draw.text((213,72), 'kcal', font = self.fonts['font15'], fill = 0x0)
+        self.draw.ellipse((15, 29, 77, 91), outline=0x0, fill = 0xFF)
+        self.draw.ellipse((29, 43, 63, 77), outline=0x0, fill = 0xFF)
+
+        # logo
+        name = "UTAGE.GAMES" # あなたのチャンネル名に変更してください（任意）
+        name_width = self.fonts['font15bold'].getlength(name)
+        self.draw.rectangle([((246 - name_width - 6), 4),(246,23)], fill = 0x0)
+        self.draw.text(((246 - name_width - 2), 4), name, font = self.fonts['font15bold'], fill = 0xFF)
+
 
     def load_data(self):
         """Load Json Data"""
@@ -102,20 +117,29 @@ class CyclingEPDDisplay:
         try:
             if data != self.last_data:
                 # draw time
-                self.draw.rectangle([(45,105),(105,120)], fill = 0xFF)
-                self.draw.text((45,105), data['elapsed_time'], font = self.fonts['font15'], fill = 0)
+                self.draw.rectangle([(184, 101),(244, 116)], fill = 0xFF)
+                self.draw.text((184, 101), data['elapsed_time'], font = self.fonts['font15bold'], fill = 0x0)
 
                 # self.draw speed
-                self.draw.rectangle([(70,0),(105,15)], fill = 0xFF)
-                self.draw.text((70,0), f"{data['speed']:4.1f}", font = self.fonts['font15'], fill = 0)
+                self.draw.rectangle([(70,101),(100, 116)], fill = 0xFF)
+                self.draw.text((70, 101), f"{data['speed']:4.1f}", font = self.fonts['font15bold'], fill = 0x0)
 
                 # self.draw distance
-                self.draw.rectangle([(190,0),(230,15)], fill = 0xFF)
-                self.draw.text((190,0), f"{data['distance']:5.2f}", font = self.fonts['font15'], fill = 0)
+                self.draw.rectangle([(40, 6),(78, 21)], fill = 0xFF)
+                self.draw.text((40, 6), f"{data['distance']:5.2f}", font = self.fonts['font15bold'], fill = 0x0)
 
                 # self.draw cal
-                self.draw.rectangle([(0,45),(120,95)], fill = 0xFF)
-                self.draw.text((0,45), f"{data['calories']:6.1f}", font = self.fonts['font40'], fill = 0)
+                self.draw.rectangle([(89, 47),(209, 87)], fill = 0xFF)
+                self.draw.text((89, 47), f"{data['calories']:6.1f}", font = self.fonts['font40bold'], fill = 0x0)
+
+                # self.draw cadence
+                self.draw.ellipse((15, 29, 77, 91), outline=0x0, fill=0xFF)
+                max_cadence = 120  # 想定する最大cadence値
+                cadence_ratio = min(data['cadence'] / max_cadence, 1.0)  # 0-1の範囲に正規化
+                end_angle = -90 + (360 * cadence_ratio)
+                self.draw.pieslice([(15, 29), (77, 91)], start=-90, end=end_angle, fill=0x0)
+                self.draw.ellipse((29, 43, 63, 77), outline=0x0, fill=0xFF)
+                self.draw.text((31, 54), f"{data['cadence']:5.1f}", font = self.fonts['font12'], fill = 0x0)
 
                 self.epd.displayPartial(self.epd.getbuffer(self.image))
                 self.last_data = data.copy()
